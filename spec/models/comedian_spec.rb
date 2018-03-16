3554f3d5391c9cd504efcfd2c123a92071055e9c
pollockcl <pollock.clee@gmail.com>
2018-03-16 10:20:54 -0600

    corrected tests for the model

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -1,12 +1,38 @@
-RSpec.describe 'As an end-user' do
-  describe 'when I visit' do
-    describe '/comedians' do
-      it 'displays names with ages' do
-        visit '/comedians'
+RSpec.describe 'When using CRUD' do
+  describe 'I can' do
+    it 'CREATE' do
+      Comedian.create(name: 'Darth Plagueis the Wise', age: '999')
 
-        expect(page).to have_content('name')
-        expect(page).to have_content('age')
-      end
+      expect(Comedian.all.size).to eq(1)
+    end
+
+    it 'not CREATE with missing attributes' do
+      com1 = Comedian.create(name: 'error')
+      com2 = Comedian.create(age: 80085)
+
+      expect(com1).to_not be_valid
+      expect(com2).to_not be_valid
+    end
+
+    it 'READ' do
+      Comedian.create(name: 'Darth Plagueis the Wise', age: 999)
+
+      expect(Comedian.find(1).name).to eq('Darth Plagueis the Wise')
+      expect(Comedian.find(1).age).to eq(999)
+    end
+
+    it 'UPDATE' do
+      Comedian.create(name: 'Darth Plagueis the Wise', age: 999)
+      Comedian.update(1, name: 'Plagueis')
+
+      expect(Comedian.find(1).name).to eq('Plagueis')
+    end
+
+    it 'DELETE' do
+      Comedian.create(name: 'Darth Plagueis the Wise', age: 999)
+      Comedian.delete(1)
+
+      expect(Comedian.all.size).to eq(0)
     end
   end
 end
