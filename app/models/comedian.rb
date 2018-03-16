5afc7076b9214e9f16059a70ac00831c51a03557
pollockcl <pollock.clee@gmail.com>
2018-03-16 10:42:37 -0600

    added average_age method along with a relationship to specials

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,4 +1,9 @@
 class Comedian < ActiveRecord::Base
   validates :name, presence: true
   validates :age, presence: true
+  has_many :specials
+
+  def self.average_age
+    average('age')
+  end
 end
