f9146e76f69b253c4b77ec1fcfb72ab24bb314bc
pollockcl <pollock.clee@gmail.com>
2018-03-16 10:31:31 -0600

    added more thorough testing

--- spec/features/comedians_page_spec.rb
+++ spec/features/comedians_page_spec.rb
@@ -2,10 +2,15 @@ RSpec.describe 'As an end-user' do
   describe 'when I visit' do
     describe '/comedians' do
       it 'displays names with ages' do
+        Comedian.create(name: 'Darth Plagueis the Wise', age: 999)
+        Comedian.create(name: 'Farquad', age: 30)
+
         visit '/comedians'
 
-        expect(page).to have_content('name')
-        expect(page).to have_content('age')
+        expect(page).to have_content('Name')
+        expect(page).to have_content('Age')
+        expect(page).to have_content('Farquad')
+        expect(page).to have_content('999')
       end
     end
   end
