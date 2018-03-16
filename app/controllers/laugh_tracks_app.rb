4975980d0d36d582aa76ded603b73619ee7a2898
pollockcl <pollock.clee@gmail.com>
2018-03-16 10:42:58 -0600

    added average age variable

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -2,6 +2,7 @@ class LaughTracksApp < Sinatra::Base
 
   get '/comedians' do
     comedians = Comedian.all
+    average_age = Comedian.average_age
 
     erb :comedians, locals: { comedians: comedians }
   end
